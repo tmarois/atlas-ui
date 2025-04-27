b264300c6370dbd12d8197badd88adf367c38211
Tim Marois <timothymarois@gmail.com>
2025-04-27 10:00:31 -0400

    Removal of usePageProp for now

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 
 import { useModal } from './composables/useModal'
 import { useTheme } from './composables/useTheme'
-import { usePageProp } from './composables/inertia/usePageProp'
 
 export {
-    usePageProp,
     useModal,
     useTheme
 }
