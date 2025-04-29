540d8a316fd0041e8525ddfb0714977ed899bd8d
Tim Marois <timothymarois@gmail.com>
2025-04-28 22:07:35 -0400

    Added tooltip for helper text

---
+++ src/components/Atlas/Help/Tooltip.vue
@@ -0,0 +1,22 @@
+<template>
+    <IconHelpCircle
+        v-tooltip.bottom="{
+            value: text,
+            pt: {
+                root: 'absolute shadow-md p-fadein py-0 -mt-1 px-0 max-w-[260px] !z-[99999]',
+                text: 'text-sm p-2 bg-gray-900 text-white rounded whitespace-pre-line'
+            }
+        }"
+        class="!size-5 fill-gray-400 stroke-white hover:fill-gray-700 cursor-help inline"
+    />
+</template>
+
+<script setup>
+import { IconHelpCircle } from '@tabler/icons-vue';
+const { text } = defineProps({
+    text: {
+        type: [String, null],
+        default: null
+    }
+});
+</script>
