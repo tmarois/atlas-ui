905e7f18cf656b48c69b6d8f6e0dece14f967112
Tim Marois <timothymarois@gmail.com>
2025-04-29 17:24:09 -0400

    Update modal functionality

--- src/composables/useModal.js
+++ src/composables/useModal.js
@@ -1,47 +1,80 @@
 import { reactive, computed } from 'vue'
 
 const modals = reactive(new Map())
+const openListeners = new Map()
+const closeListeners = new Map()
 
 export function useModal() {
-    const openModal = (name, data = null) => {
+    const open = (name, data = null) => {
         modals.set(name, { open: true, data })
+
+        if (openListeners.has(name)) {
+            for (const callback of openListeners.get(name)) {
+                callback(data)
+            }
+        }
     }
 
-    const closeModal = (name) => {
+    const close = (name) => {
         if (modals.has(name)) {
+            const previousData = modals.get(name)?.data || null
             modals.set(name, { ...modals.get(name), open: false })
+
+            if (closeListeners.has(name)) {
+                for (const callback of closeListeners.get(name)) {
+                    callback(previousData)
+                }
+            }
         }
     }
 
-    const closeAllModals = () => {
+    const closeAll = () => {
         for (const [name, modal] of modals.entries()) {
-            modals.set(name, { ...modal, open: false })
+            if (modal.open) {
+                close(name)
+            }
         }
     }
 
-    const modalActiveState = (name) => computed({
+    const activeState = (name) => computed({
         get: () => modals.get(name)?.open === true,
         set: (value) => {
-            if (value) openModal(name)
-            else closeModal(name)
+            if (value) open(name)
+            else close(name)
         }
     })
 
-    const getModalData = (name) => computed(() => modals.get(name)?.data || null)
+    const data = (name) => computed(() => modals.get(name)?.data || null)
 
-    const isAnyModalOpen = computed(() => {
+    const isOpen = computed(() => {
         for (const modal of modals.values()) {
             if (modal.open) return true
         }
         return false
     })
 
+    const onOpen = (name, callback) => {
+        if (!openListeners.has(name)) {
+            openListeners.set(name, [])
+        }
+        openListeners.get(name).push(callback)
+    }
+
+    const onClose = (name, callback) => {
+        if (!closeListeners.has(name)) {
+            closeListeners.set(name, [])
+        }
+        closeListeners.get(name).push(callback)
+    }
+
     return {
-        openModal,
-        closeModal,
-        closeAllModals,
-        modalActiveState,
-        getModalData,
-        isAnyModalOpen,
+        open,
+        close,
+        closeAll,
+        activeState,
+        data,
+        isOpen,
+        onOpen,
+        onClose,
     }
 }
