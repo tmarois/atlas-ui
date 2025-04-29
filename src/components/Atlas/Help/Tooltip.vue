1d317afb78f5b4a4dd3d0e6901d0e15d704210cc
Tim Marois <timothymarois@gmail.com>
2025-04-29 19:58:37 -0400

    Clean up styles on tooltips

--- src/components/Atlas/Help/Tooltip.vue
+++ src/components/Atlas/Help/Tooltip.vue
@@ -1,22 +1,27 @@
 <template>
     <IconHelpCircle
-        v-tooltip.bottom="{
+        v-tooltip.top="{
             value: text,
             pt: {
-                root: 'absolute shadow-md p-fadein py-0 -mt-1 px-0 max-w-[260px] !z-[99999]',
+                root: 'absolute shadow-md p-fadein py-0 px-0 max-w-[260px]',
                 text: 'text-sm p-2 bg-surface-900 text-white dark:bg-surface-700 rounded whitespace-pre-line'
             }
         }"
-        class="!size-5 fill-surface-400 stroke-white hover:fill-surface-700 cursor-help inline dark:!fill-surface-300 dark:hover:!fill-surface-400 dark:stroke-surface-900"
+        class="fill-surface-400 stroke-white hover:fill-surface-700 cursor-help inline dark:!fill-surface-300 dark:hover:!fill-surface-400 dark:stroke-surface-900"
+        :class="iconClass"
     />
 </template>
 
 <script setup>
 import { IconHelpCircle } from '@tabler/icons-vue';
-const { text } = defineProps({
+const { text, iconClass } = defineProps({
     text: {
         type: [String, null],
         default: null
+    },
+    iconClass: {
+        type: String,
+        default: 'size-6'
     }
 });
 </script>
