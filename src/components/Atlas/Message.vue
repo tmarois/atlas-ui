de82179e34cdc47eec6ece57b1f84b7cd5108a5c
Tim Marois <timothymarois@gmail.com>
2025-04-30 16:36:12 -0400

    Update Message.vue

--- src/components/Atlas/Message.vue
+++ src/components/Atlas/Message.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="p-4 flex border items-center space-x-2 rounded"
+        class="p-4 flex border items-center space-x-4 rounded"
         :class="{
             'bg-surface-50 border-surface-200 dark:bg-surface-700 dark:border-surface-600 dark:text-surface-100': !warning,
             'bg-yellow-100 border-yellow-300 dark:bg-yellow-700 dark:border-yellow-600 dark:text-surface-100': warning
@@ -12,8 +12,11 @@
                 <IconInfoCircleFilled v-else class="size-8 text-surface-600 dark:text-surface-400" />
             </slot>
         </div>
-        <div class="w-full text-sm">
+        <div class="flex-1 w-full text-sm">
             <slot />
+        </div>
+        <div v-if="$slots.actions" class="flex items-center space-x-2">
+            <slot name="actions" />
         </div>
     </div>
 </template>
