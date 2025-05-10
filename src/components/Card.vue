48d86d3d6ac327b8bdc556b20aff157fae639673
Tim Marois <timothymarois@gmail.com>
2025-05-10 14:03:09 -0500

    Update Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -25,7 +25,7 @@ const theme = ref<CardPassThroughOptions>({
         bg-surface-0 dark:bg-surface-800
         text-surface-700 dark:text-surface-0
         border border-surface-300 dark:border-surface-700
-        w-full h-full shadow-sm`,
+        w-full h-full shadow-[0_1px_2px_0_rgba(0,0,0,0.07)]`,
     header: `p-6 py-4 border-b border-surface-300 dark:border-surface-700`,
     body: `flex flex-col space-y-4`,
     caption: `flex flex-col gap-2`,
