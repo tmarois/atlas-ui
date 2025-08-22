b7c3da0ae8d439ad13c3acb5f3af332df46da7f6
Tim Marois <timothymarois@gmail.com>
2025-08-22 15:05:04 -0400

    feat(ui): add checkbox, input number and mask components

--- ui/src/components/index.ts
+++ ui/src/components/index.ts
@@ -1,5 +1,8 @@
 export { default as AButton } from './AButton.vue';
 export { default as ACard } from './ACard.vue';
+export { default as ACheckbox } from './ACheckbox.vue';
+export { default as AInputMask } from './AInputMask.vue';
+export { default as AInputNumber } from './AInputNumber.vue';
 export { default as AInputText } from './AInputText.vue';
 export { default as ASelect } from './ASelect.vue';
 export { default as ATextarea } from './ATextarea.vue';
