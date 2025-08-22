c7bbe42821f2f42ab8904680433dcb34fa1fb6d7
Tim Marois <timothymarois@gmail.com>
2025-08-22 14:50:19 -0400

    feat(ui): add Select and Card components

---
+++ ui/src/components/ACard.vue
@@ -0,0 +1,38 @@
+<template>
+    <Card
+        unstyled
+        :pt="theme"
+        :ptOptions="{
+            mergeProps: ptViewMerge
+        }"
+    >
+        <template v-for="(_, slotName) in $slots" v-slot:[slotName]="slotProps">
+            <slot :name="slotName" v-bind="slotProps ?? {}" />
+        </template>
+    </Card>
+</template>
+
+<script setup lang="ts">
+import Card, { type CardPassThroughOptions, type CardProps } from 'primevue/card';
+import { ref } from 'vue';
+import { ptViewMerge } from '../utils';
+
+interface Props extends /* @vue-ignore */ CardProps {}
+defineProps<Props>();
+
+const theme = ref<CardPassThroughOptions>({
+    root: `flex flex-col rounded-lg
+        bg-surface-0 dark:bg-surface-800
+        text-surface-700 dark:text-surface-0
+        border border-surface-300 dark:border-surface-700
+        w-full h-full shadow-[0_1px_2px_0_rgba(0,0,0,0.07)] overflow-hidden`,
+    header: `p-6 py-4 border-b border-surface-300 dark:border-surface-700`,
+    body: `flex flex-col`,
+    caption: `flex flex-col gap-2`,
+    title: `font-semibold text-lg`,
+    subtitle: `text-surface-500 dark:text-surface-400`,
+    content: `p-6 h-full`,
+    footer: `p-6 py-4 border-t border-surface-300 dark:border-surface-700`
+});
+</script>
+
--- ui/src/components/index.ts
+++ ui/src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as AButton } from './AButton.vue';
+export { default as ACard } from './ACard.vue';
+export { default as ASelect } from './ASelect.vue';
