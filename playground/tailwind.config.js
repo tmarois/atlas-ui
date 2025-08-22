d68f1fdda7064246ffe856f39d08555a72fbfb5a
Tim Marois <timothymarois@gmail.com>
2025-08-22 15:46:04 -0400

    fix(playground): ensure tailwind sources include ui

--- playground/tailwind.config.js
+++ playground/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import primeui from 'tailwindcss-primeui'
+
 export default {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
-    '../ui/src/**/*.{vue,js,ts,jsx,tsx}',
   ],
+  plugins: [primeui],
 }
