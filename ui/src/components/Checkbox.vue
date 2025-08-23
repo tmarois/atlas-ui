efb25179338b807dd49211215bde3e71398b5a3e
Tim Marois <timothymarois@gmail.com>
2025-08-23 10:45:00 -0400

    fix: align checkbox with label

--- ui/src/components/Checkbox.vue
+++ ui/src/components/Checkbox.vue
@@ -24,7 +24,7 @@ const props = defineProps<Props>();
 const attrs = useAttrs();
 
 const theme = ref<CheckboxPassThroughOptions>({
-    root: `relative inline-flex select-none w-5 h-5 align-bottom
+    root: `relative flex select-none w-5 h-5
         p-small:w-4 p-small:h-4
         p-large:w-6 p-large:h-6`,
     input: `peer cursor-pointer disabled:cursor-default appearance-none
@@ -33,14 +33,13 @@ const theme = ref<CheckboxPassThroughOptions>({
     box: `flex justify-center items-center rounded-sm w-5 h-5
         border border-surface-300 dark:border-surface-700
         bg-surface-0 dark:bg-surface-950
-        text-surface-700 dark:text-surface-0
         peer-enabled:peer-hover:border-surface-500 dark:peer-enabled:peer-hover:border-surface-600
-        p-checked:border-primary-400 p-checked:bg-primary-500 p-checked:text-primary-contrast
+        p-checked:border-primary-400 p-checked:bg-primary-500
         peer-enabled:peer-hover:p-checked:bg-primary-500/70 peer-enabled:peer-hover:p-checked:border-primary-emphasis
         peer-focus-visible:outline-1 peer-focus-visible:outline-offset-2 peer-focus-visible:outline-primary peer-focus-visible:outline
         p-invalid:border-red-400 dark:p-invalid:border-red-500
         p-filled:bg-surface-50 dark:p-filled:bg-surface-800
-        p-disabled:bg-surface-200 dark:p-disabled:bg-surface-400 p-disabled:border-surface-300 dark:p-disabled:border-surface-500 p-disabled:text-surface-700 dark:p-disabled:text-surface-400
+        p-disabled:bg-surface-200 dark:p-disabled:bg-surface-400 p-disabled:border-surface-300 dark:p-disabled:border-surface-500
         shadow-[0_1px_2px_0_rgba(18,18,23,0.05)] transition-colors duration-200
         p-small:w-4 p-small:h-4
         p-large:w-6 p-large:h-6`,
