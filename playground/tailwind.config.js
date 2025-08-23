aadee727c069bdd0c328babc9673019076173729
Tim Marois <timothymarois@gmail.com>
2025-08-23 09:00:00 -0400

    fix: ensure tailwind styles load in playground

--- playground/tailwind.config.js
+++ playground/tailwind.config.js
@@ -3,6 +3,6 @@ export default {
   content: [
       './index.html',
       './src/**/*.{vue,js,ts,jsx,tsx}',
-      '../src/**/*.{vue,js,ts,jsx,tsx}',
+      '../ui/src/**/*.{vue,js,ts,jsx,tsx}',
   ],
 }
