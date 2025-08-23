d738ef66c6ed7dde050f5350b1a40d03b54d1e6b
Tim Marois <timothymarois@gmail.com>
2025-08-23 13:13:04 -0400

    chore: elevate tooltip overlay z-index

--- ui/src/components/TooltipIcon.vue
+++ ui/src/components/TooltipIcon.vue
@@ -38,14 +38,16 @@ const mergedPt = computed(() => ptMerge(theme.value, props.pt));
 
 const tooltipTheme = ref<TooltipDirectivePassThroughOptions>({
     root: {
-        class: 'absolute shadow-md p-fadein py-0 px-0 max-w-[260px]',
-        style: 'z-index: 10000 !important'
+        class: 'absolute shadow-md p-fadein py-0 px-0 max-w-[260px]'
     },
     text: 'text-sm p-2 border border-surface-700 bg-surface-900 text-white dark:bg-surface-700 dark:border-surface-800 rounded whitespace-pre-line'
 });
 
 const tooltip = computed(() => ({
     value: props.text,
+    appendTo: 'body',
+    autoZIndex: true,
+    baseZIndex: 20000,
     pt: ptMerge(tooltipTheme.value, props.pt?.tooltip)
 }));
 </script>
