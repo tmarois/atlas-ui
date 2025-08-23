a1ebb5ba035acc7d8aae247f14d959f6a3c1c3e2
Tim Marois <timothymarois@gmail.com>
2025-08-23 11:20:10 -0400

    fix(playground): load primeui tailwind plugin

--- playground/tailwind.config.js
+++ playground/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import primeui from 'tailwindcss-primeui';
+
 export default {
   content: [
       './index.html',
       './src/**/*.{vue,js,ts,jsx,tsx}',
       '../ui/src/**/*.{vue,js,ts,jsx,tsx}',
   ],
+  plugins: [primeui],
 }
