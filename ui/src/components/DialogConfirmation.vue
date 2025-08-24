b4c44853f7c43d1e5e3dcc6cd319b93eb59e39bd
Tim Marois <timothymarois@gmail.com>
2025-08-24 08:01:30 -0400

    fix(ui): correct confirm button pass-through in DialogConfirmation

--- ui/src/components/DialogConfirmation.vue
+++ ui/src/components/DialogConfirmation.vue
@@ -80,7 +80,9 @@ const theme = computed<DialogConfirmationPassThroughOptions>(() => ({
     message: 'text-gray-700 dark:text-gray-200',
     actions: 'flex justify-center items-center space-x-4 py-2',
     confirmButton: {
-        root: 'text-white bg-red-600 enabled:hover:bg-red-700 border-red-600 enabled:active:border-red-400 enabled:active:bg-red-600 enabled:hover:border-red-emphasis',
+        root: {
+            class: 'text-white bg-red-600 enabled:hover:bg-red-700 border-red-600 enabled:active:border-red-400 enabled:active:bg-red-600 enabled:hover:border-red-emphasis',
+        },
     },
     cancelButton: {},
 }));
